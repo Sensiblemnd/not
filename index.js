3fd6522a9f553387d80c2a446d98c2527563b028
Rob Lloveras <Sensiblemnd@users.noreply.github.com>
2019-08-27 16:05:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-//Creates a function out of '!'.  
-//Use this to improve code readability.  
-//Instead of...     if (!loggedIn) {}   
-//..do this..   if not(loggedIn) {}
 const not = value => !value;
 
 export default not;
